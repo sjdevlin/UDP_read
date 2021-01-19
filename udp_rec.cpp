13137186407191aa0d9b81d0cebcacf111377bb9
Stephen Devlin <sjdevlin@gmail.com>
2021-01-19 11:07:30 +0000

    Add files via upload

---
+++ udp_rec.cpp
@@ -0,0 +1,31 @@
+udp_server::udp_server(const std::string& addr, int port)
+    : f_port(port)
+    , f_addr(addr)
+{
+    char decimal_port[16];
+    snprintf(decimal_port, sizeof(decimal_port), "%d", f_port);
+    decimal_port[sizeof(decimal_port) / sizeof(decimal_port[0]) - 1] = '\0';
+    struct addrinfo hints;
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_DGRAM;
+    hints.ai_protocol = IPPROTO_UDP;
+    int r(getaddrinfo(addr.c_str(), decimal_port, &hints, &f_addrinfo));
+    if(r != 0 || f_addrinfo == NULL)
+    {
+        throw udp_client_server_runtime_error(("invalid address or port for UDP socket: \"" + addr + ":" + decimal_port + "\"").c_str());
+    }
+    f_socket = socket(f_addrinfo->ai_family, SOCK_DGRAM | SOCK_CLOEXEC, IPPROTO_UDP);
+    if(f_socket == -1)
+    {
+        freeaddrinfo(f_addrinfo);
+        throw udp_client_server_runtime_error(("could not create UDP socket for: \"" + addr + ":" + decimal_port + "\"").c_str());
+    }
+    r = bind(f_socket, f_addrinfo->ai_addr, f_addrinfo->ai_addrlen);
+    if(r != 0)
+    {
+        freeaddrinfo(f_addrinfo);
+        close(f_socket);
+        throw udp_client_server_runtime_error(("could not bind UDP socket with: \"" + addr + ":" + decimal_port + "\"").c_str());
+    }
+}
